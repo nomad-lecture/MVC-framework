fc78705dc309c6521fd8a20901cced0eda29e320
Bilguun NoAmd <nomad.lecture@gmail.com>
2018-08-02 16:24:15 +0800

    First commit

--- app/config/config.php
+++ app/config/config.php
@@ -1,18 +1,18 @@
 <?php
     //DB Params
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'shareposts');
+    define('DB_HOST', '__YOURHOST___');
+    define('DB_USER', '__DB USER__');
+    define('DB_PASS', '__ DB PASS __');
+    define('DB_NAME', '__DB NAME ___');
 
     //App Root
     define('APPROOT', dirname(dirname(__FILE__)));
 
     //URL Root
-    define('URLROOT', 'http://localhost/mvcsandbox');
+    define('URLROOT', '__SITE URL ___');
 
     //Site Name
-    define('SITENAME', 'TraversyMVC');
+    define('SITENAME', '___NAME___');
 
     //App version
     define('APPVERSION', '1.0.0');
